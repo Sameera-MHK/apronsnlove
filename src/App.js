540abcf980f83acaf4bb27b72fc4dc5a95afb0a5
Sameera-MHK <mhksameera@gmail.com>
2023-07-17 12:36:57 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
 import "./App.scss";
-
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Products from "./pages/Products/Products";
-import Product from "./pages/Product/Product";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import Error from "./pages/Error/Error";
-import Almond from "./pages/Almond/Almond";
-import Coconut from "./pages/Almond/Coconut";
-import Chocolate from "./pages/NutButter/Chocolate";
-import Nut_Butters from "./pages/NutButter/Nut_Butters";
-import Contact from "./pages/Contact/Contact";
-import Mission from "./pages/Mission/Mission";
-import About from "./pages/About/About";
-import Conditions from "./pages/t&c/Conditions";
-import PrivacyPolicy from "./pages/Policy/PrivacyPolicy";
+import Home from "./pages/Home/Home.jsx";
+import Products from "./pages/Products/Products.jsx";
+import Product from "./pages/Product/Product.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import Error from "./pages/Error/Error.jsx";
+import Almond from "./pages/Almond/Almond.jsx";
+import Coconut from "./pages/Almond/Coconut.jsx";
+import Chocolate from "./pages/NutButter/Chocolate.jsx";
+import Nut_Butters from "./pages/NutButter/Nut_Butters.jsx";
+import Contact from "./pages/Contact/Contact.jsx";
+import Mission from "./pages/Mission/Mission.jsx";
+import About from "./pages/About/About.jsx";
+import Conditions from "./pages/t&c/Conditions.jsx";
+import PrivacyPolicy from "./pages/Policy/PrivacyPolicy.jsx";
 
 // pages for the router
 
